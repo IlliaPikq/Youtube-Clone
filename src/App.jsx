0bb38884edfaae9999357250c4de7c9b9da93d88
Illia <illiapikhotski@gmail.com>
2025-01-22 23:51:09 +0100

    made routing and added constants

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  ChannelDetail,
+  Feed,
+  Navbar,
+  SearchFeed,
+  VideoDetail,
+} from "./components";
+
 function App() {
-  return <h1>Hello Youtube Clone!</h1>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path={"/"} element={<Feed />}></Route>
+        <Route path={"/video/:id"} element={<VideoDetail />}></Route>
+        <Route path={"/channel/:id"} element={<ChannelDetail />}></Route>
+        <Route path={"/search/:searchTerm"} element={<SearchFeed />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
