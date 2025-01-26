1f4ba4fd064dbd42e9c69476fb15864cb413bc83
Illia <illiapikhotski@gmail.com>
2025-01-26 23:35:33 +0100

    udate

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -24,7 +24,7 @@ const ChannelCard = ({ channelDetail, marginTop }) => (
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
-          textAlign: "center",
+          alignItems: "center",
           color: "#fff",
         }}
       >
@@ -48,7 +48,13 @@ const ChannelCard = ({ channelDetail, marginTop }) => (
           />
         </Typography>
         {channelDetail?.statistics?.subscriberCount && (
-          <Typography sx={{ fontSize: "15px", fontWeight: 500, color: "gray" }}>
+          <Typography
+            sx={{
+              fontSize: "15px",
+              fontWeight: 500,
+              color: "gray",
+            }}
+          >
             {parseInt(
               channelDetail?.statistics?.subscriberCount
             ).toLocaleString("en-US")}{" "}
--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,7 +1,44 @@
-import React from "react";
-
+import { Box } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchApi } from "../utils/fetchApi";
+import ChannelCard from "./ChannelCard";
+import Videos from "./Videos";
 const ChannelDetail = () => {
-  return <div>ChannelDetail</div>;
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [channelVideos, setChannelVideos] = useState([]);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchApi(`channels?part=snippet&id=${id}`).then((data) => {
+      setChannelDetail(data?.items[0]);
+    });
+    fetchApi(`search?channelId=${id}&part=snippet&order=date`).then((data) =>
+      setChannelVideos(data?.items)
+    );
+  }, [id]);
+
+  return (
+    <Box minHeight="95vh" sx={{ backgroundColor: "#000" }}>
+      <Box>
+        <div
+          style={{
+            height: "300px",
+            background: "rgb(36,0,0)",
+            background:
+              "linear-gradient(90deg, rgba(36,0,0,1) 0%, rgba(121,9,15,1) 35%, rgba(255,0,31,1) 100%)",
+          }}
+        ></div>
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+      </Box>
+      <Box display={"flex"} p={2}>
+        <Box sx={{ mr: { sm: "100px" } }}>
+          <Videos videos={channelVideos} />
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default ChannelDetail;
